656b80f2c6d23b09adc9c8e07088755684a4d3f6
Fernando Chagas <fnd.chagas@gmail.com>
2026-01-08 12:12:42 -0300

    Add Expo Go drift workflow

---
+++ src/expo/constants.ts
@@ -0,0 +1,4 @@
+export const EXPO_GO_ANDROID_URL =
+  "https://github.com/expo/expo-go-releases/releases/download/Expo-Go-54.0.6/Expo-Go-54.0.6.apk";
+
+export const EXPO_GO_VERSIONS_API = "https://exp.host/--/api/v2/versions/latest";
