3d26811b2f48d2be942586b6d45471f36dfe65a2
Fernando Chagas <fnd.chagas@gmail.com>
2026-08-05 09:59:41 -0300

    fix: update pinned Expo Go to 57.0.3 and dedupe drift issues

--- src/expo/constants.ts
+++ src/expo/constants.ts
@@ -1,4 +1,4 @@
 export const EXPO_GO_ANDROID_URL =
-  "https://github.com/expo/expo-go-releases/releases/download/Expo-Go-54.0.6/Expo-Go-54.0.6.apk";
+  "https://github.com/expo/expo-go-releases/releases/download/Expo-Go-57.0.3/Expo-Go-57.0.3.apk";
 
 export const EXPO_GO_VERSIONS_API = "https://exp.host/--/api/v2/versions/latest";
